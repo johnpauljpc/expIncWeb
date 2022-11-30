7de9ef852065ffd4ec8e8fd402341603b8f6b045
johnpauljpc <jpcwork081@gmail.com>
2022-11-30 06:58:38 +0100

    tweaked

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import registrationView
 
 urlpatterns = [
-    path('register/', registrationView.as_view(), name='register')
+    path('register/', registrationView.as_view(), name='register'),
 ]
