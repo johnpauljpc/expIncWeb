780d88d7c2cdf4138db96f9c6705750412f92e25
johnpauljpc <jpcwork081@gmail.com>
2023-01-29 22:05:06 +0100

    setTimeout

--- static/js/searchField.js
+++ static/js/searchField.js
@@ -38,7 +38,7 @@ searchField.addEventListener("keyup", (e) => {
                     <td>${item.description}</td>
                     <td>${item.date}</td>
                     <td> <a href="edit/${item.id}" class="btn btn-sm btn-warning">Edit</a></td>
-                    <td> <a href="delete/${item.id}" class="btn btn-sm btn-danger">Delete</a></td>
+                    
                     </tr>
                     `;
           });
