9edf1466afa1ce9a88b07f55965aed15a33a25e3
johnpauljpc <jpcwork081@gmail.com>
2023-07-18 20:02:05 +0100

    .

--- website/settings.py
+++ website/settings.py
@@ -86,13 +86,17 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'incomeExpense',
-        'USER': 'postgres',
-        'PASSWORD': '08109137270',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'incomeExpense',
+    #     'USER': 'postgres',
+    #     'PASSWORD': '08109137270',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
+    'default':{
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
